07456bf5775a01178a7dc72c8ba396bc89afb9ab
nicolasult <nicolas.ultrillo@gmail.com>
2025-06-08 22:01:23 +0200

    Add player class and get input to make it move

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pygame, sys
 from constants import *
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-if __name__ == "__main__":
-    main()
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +16,15 @@ if __name__ == "__main__":
                 pygame.quit()
                 sys.exit()
         
+        player.update(dt)
+        
         screen.fill("black")
+        player.draw(screen)
 
         pygame.display.flip()
+
+        dt = clock.tick() / 1000
+
+if __name__ == "__main__":
+    main()
+    
