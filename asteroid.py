e71f1494976948df5166129e24efe67a7eec17a2
nicolasult <nicolas.ultrillo@gmail.com>
2025-06-13 23:31:05 +0200

    Add rotations variables in the split method

--- asteroid.py
+++ asteroid.py
@@ -17,5 +17,7 @@ class Asteroid(CircleShape):
     def split(self):
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS: return
-        else:
-            pass
+        random_angle = random.uniform(20, 50)
+
+        dir1 = self.velocity.rotate(random_angle)
+        dir2 = self.velocity.rotate(-random_angle)
